310d892229c83feaf220451dfb8dd1c0f63c1ecf
leon <leonlnguyen93@gmail.com>
2024-11-03 01:30:52 -0500

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,19 +2,20 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import { 
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious     
+ } from "~/components/ui/carousel";
 
 export default function Page() {
-    const [isHovered, setHovered] = useState<boolean>(false);
-
     return (
         <>
-            <div>select game</div>
+            <Carousel>
 
-            <Link href={"/play/dance"}>
-                <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-                    {isHovered ? "> dance" : "dance"}
-                </div>
-            </Link>
+            </Carousel>
         </>
     )
 
