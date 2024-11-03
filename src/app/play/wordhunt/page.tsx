47e372fcd5f6fa1e4ec3043f57f94509e9b35351
Ashley <ashleyoftexas@gmail.com>
2024-11-03 10:28:52 -0600

    no hover

--- src/app/play/wordhunt/page.tsx
+++ src/app/play/wordhunt/page.tsx
@@ -3,26 +3,24 @@
 import Link from "next/link";
 import { useState } from "react";
 import { MainComponent } from "~/app/_components/Main";
-        
+
 import WordHuntGame from "~/app/_components/WordHuntGame";
 import useSound from "use-sound";
 
 export default function WordHunt() {
   const [userPose, setUserPose] = useState<any[]>([]);
   const [handPose, setHandPose] = useState<any[]>([]);
-  
-  const [backSound] = useSound("/audio/select.wav")
+
+  const [backSound] = useSound("/audio/select.wav");
 
   return (
     <div className="flex h-screen w-full flex-col">
       <Link href="/play">
         <div
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
           className="fixed left-6 top-6 z-50 hover:translate-y-1"
           onClick={() => backSound()}
         >
-          {isHovered ? "< back" : "back"}
+          Back
         </div>
       </Link>
       <div className="flex">
